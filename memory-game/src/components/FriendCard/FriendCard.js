ae6aa32d54babd9eaccfa2caa4aafb601acbf316
Rob McCracken <rob.mccrackenjr@gmail.com>
2018-03-28 15:31:10 -0400

    setting up component structure & updated JSON

---
+++ memory-game/src/components/FriendCard/FriendCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./FriendCard.css";
+
+const FriendCard = props => (
+  <div className="card">
+    <div className="img-container">
+      <img alt={props.name} src={props.image} />
+    </div>
+    <span onClick={() => props.removeFriend(props.id)} className="remove">
+    </span>
+  </div>
+);
+
+export default FriendCard;
